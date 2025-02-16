effe9960859592571a0e4ef7956f4151f06380f5
Kathy <gkathysanchez@gmail.com>
2025-02-16 13:01:56 -0500

    started data cleaning

--- trace_scraper.py
+++ trace_scraper.py
@@ -4,6 +4,8 @@ import pandas as pd
 import time  # Add delays between requests
 import requests
 import os
+import re
+import numpy as np
 
 print(os.getcwd())
 os.chdir('/Volumes/Mac_Passport/projects/personal/atf')
@@ -32,8 +34,6 @@ years = ["2023"]
 
 # Define the webpage URL
 base_url = "https://www.atf.gov/resource-center/firearms-trace-data-{}-{}"
-
-
 
 all_data = []
 
@@ -75,7 +75,7 @@ for state in states:
                 
                     if th_element and td_element:  # Ensure both elements exist
                         print(f"Found th: {th_element}, td: {td_element}")                        
-                        category = th_element.text.strip().replace(" ", "_")  # Extract the text only
+                        category = th_element.text.strip() # Extract the text only
                         value = td_element.text.strip().replace(",", "")  # Extract and clean the text from td
                 
                         state = state.replace("-", " ").title()
@@ -84,7 +84,7 @@ for state in states:
                             "Year": year,
                             #"State": url.split("/")[-1].split("-")[0].capitalize(),  # Extract state from URL
                             #"Year": url.split("/")[-1].split("-")[1],  # Extract year from URL
-                            "Section": section_title,  
+                            "Section_raw": section_title,  
                             "Category_or_Location": category,  # Header 
                             "Value": value  # Count or other value
                             }
@@ -94,19 +94,38 @@ for state in states:
                         print("No valid <th> or <td> in this row.")
             else:
                 print(f"{state} {year}: No table found for {section_title}.")
+            
+            # Find the paragraph that is immediately after the table
+            note = table.find_next_sibling("p") if table else None 
+            
+            if note:
+                print(f'{state} {year} found note: {note}')
+                row_dict["Note"] = note.text.strip() 
+            else:
+                print(f"{state} {year}: No note found under {section_title}.")
+                row_dict["Note"] = np.nan
+
+            note2 = note.find_next_sibling("p") if table else None 
+            
+            if note2:
+                print(f'{state} {year} found note: {note2}')
+                row_dict["Note2"] = note2.text.strip() 
+            else:
+                print(f"{state} {year}: No note found under {section_title}.")
+                row_dict["Note2"] = np.nan
     
         time.sleep(1)  # Avoid overwhelming the server
 
 # Convert list of dictionaries to Pandas DataFrame
 df = pd.DataFrame(all_data)
 
-prepositions_articles = ['on ', 'with', 'a ', 'an ','for', 'in', 'the', 'with', '  ']
+words = ['Calendar', 'Year', '2023', '2022', '2021', 'on', 'with', 'a', 'an','for', 'in', 'the', 'with']
 
-states_title = ["Alabama", "Alabama", "Arizona", "California", "Colorado", 
+states_title_case = ["Alabama", "Alabama", "Arizona", "California", "Colorado", 
           "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia",
           "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
           "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", 
-          "Massaschusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+          "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
           "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", 
           "New Meexico", "New York", "North Carolina", "North Dakota", "Ohio", 
           "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
@@ -114,20 +133,58 @@ states_title = ["Alabama", "Alabama", "Arizona", "California", "Colorado",
           "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming",
           "Puerto Rico"]
 
-def remove_characters(text, states_title, prepositions_articles):
-    for state in states_title:
+def remove_characters(text, states_title_case, words):
+    for state in states_title_case:
+        text = text.replace(f'with an {state} Recovery', '').strip()
+        text = text.replace(f'with a {state} Recovery', '').strip()
+        text = text.replace('Firearm Traces', '').strip()
         text = text.replace(state, '').strip()
 
-    for item in prepositions_articles:
-        text = text.replace(' ' + item.lower() + ' ', ' ').strip()
+
+    for word in words:
+        #text = text.replace(' ' + word.lower() + ' ', ' ').strip()
+        text = re.sub(r'\b' + re.escape(word.strip()) + r'\b', '', text, flags=re.IGNORECASE)
     
-    # Remove any extra spaces that might appear due to replacements
-    return ' '.join(text.split())
+    text = re.sub(r'\u200b', '', text)
+    
+    #return ' '.join(text.split())
+    return re.sub(r'\s+', ' ', text).strip()
 
-# Apply the function to the 'text' column
-df['Section_clean'] = df['Section'].apply(lambda x: remove_characters(x, states_title, prepositions_articles))
 
-# Drop contents, drop disclaimer
+def rename_category_col(text):
+    text = text.replace('Recovery City', 'Top Recovery Cities')
+    text = text.replace('Top 15 Source States Firearms', 'Top 15 Source States')
+    return text
+
+# Clean data
+    
+    # Remove categories
+
+df['Section'] = df['Section_raw'].apply(lambda x: remove_characters(x, states_title_case, words))
+    
+    # Rename categories
+    
+df['Section'] = df['Section'].apply(lambda x: rename_category_col(x))
+
+    # Replace empty note cells with missing
+    
+note_cols = ['Note', 'Note2']
+for note_col in note_cols:
+    df[note_col] = df[note_col].replace("", np.nan)
+
+    # Replace notes with missing (for notes that belong to other tables)
+
+df.loc[df["Note"].isna(), "Note2"] = np.nan 
+
+    # Drop section headers that had no data
+
+values_to_remove = ["Contents", "ATF Firearms Trace Data Disclaimer"] 
+df = df[~df["Section"].isin(values_to_remove)]
+
+
+# Review categories and columns
+
+df_category_values = pd.DataFrame(df['Category_or_Location'].value_counts())
 
 # Display the first few rows
 print(df.head())
